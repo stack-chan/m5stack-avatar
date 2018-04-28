bcdc734e24dc0778c9667b6bac7081279b5b1792
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-04-28 15:37:29 +0900

    Add Copyright

--- src/avator.cpp
+++ src/avator.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) Shinya Ishikawa. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 #include "avator.h"
 
 #define PRIMARY_COLOR WHITE
--- src/avator.h
+++ src/avator.h
@@ -1,3 +1,6 @@
+// Copyright (c) Shinya Ishikawa. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 #pragma once
 #include <M5Stack.h>
 
