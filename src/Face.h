b9c310546bf266159ad9977d2a4b260ff4767442
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-05-22 00:34:10 +0900

    Update according to M5Stack 0.2.0

--- src/Face.h
+++ src/Face.h
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #pragma once
-#include "Sprite.h"
+#include <M5Stack.h> // TODO: include only the Sprite function not a whole library
 #include "Mouth.h"
 #include "Eye.h"
 #include "Eyeblow.h"
