e6a7f62e9ac7f95780e2d098c62b37673139c7d2
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-06-02 01:17:40 +0900

    Add examples

---
+++ examples/talk.ino
@@ -0,0 +1,30 @@
+#include <M5Stack.h>
+#include "Avatar.h"
+#include "const.h"
+#include <AquesTalkTTS.h>
+#include "tasks/LipSync.h"
+
+using namespace m5avatar;
+
+Avatar *avatar;
+void setup()
+{
+  int iret;
+  iret = TTS.create(AQUESTALK_KEY);
+  M5.begin();
+  M5.Lcd.setBrightness(30);
+  M5.Lcd.clear();
+  avatar = new Avatar();
+
+  avatar->init();
+  avatar->addTask(lipSync, "lipSync");
+}
+
+void loop()
+{
+  M5.update();
+  if (M5.BtnA.wasPressed())
+  {
+    TTS.play("konnichiwa", 80);
+  }
+}
