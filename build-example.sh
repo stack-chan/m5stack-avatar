c5b039213a5d88db3159edae3e33f8a26cf2d810
Shinya Ishikawa <ishikawa@rt-net.jp>
2021-05-16 23:59:51 +0900

    Pass options to pio command

--- build-example.sh
+++ build-example.sh
@@ -1,8 +1,11 @@
-# 実行時に指定された引数の数、つまり変数 $# の値が 3 でなければエラー終了。
-if [ $# -ne 1 ]; then
-  echo "usage: build-example.sh example-name" 1>&2
+#!/bin/bash
+
+echo $@
+if [ $# -lt 1 ]; then
+  echo "usage: build-example.sh example-name [options]" 1>&2
   exit 1
 fi
 rm ./src/*.ino
 cp ./examples/$1/$1.ino ./src/
-platformio run --target upload
+shift
+platformio run --target upload $@
