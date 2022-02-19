3db9bde2d6cf3095fc8c79b2e999df695236dd57
Takao Akaki <mongonta@gmail.com>
2022-02-19 19:03:55 +0900

    fix DogFace.h

--- src/faces/DogFace.h
+++ src/faces/DogFace.h
@@ -5,11 +5,7 @@
 #ifndef FACES_DOGFACE_H_
 #define FACES_DOGFACE_H_
 
-# if defined(ARDUINO_M5STACK_Core2) || defined(M5AVATAR_CORE2) || defined(_M5Core2_H_)
-  #include <M5Core2.h>
-# else
-  #include <M5Unified.h> // TODO(meganetaaan): include only the Sprite function not a whole library
-# endif
+#include <M5Unified.h> // TODO(meganetaaan): include only the Sprite function not a whole library
 #include "../BoundingRect.h"
 #include "../DrawContext.h"
 #include "../Drawable.h"
